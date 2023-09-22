08dd4f831d2421add6cddda62b8448d8440ebd1d
TsaiWeiTse <twjzi14200@gmail.com>
2023-09-22 19:05:59 +0800

    20230922

--- openapi/data_login_api.php
+++ openapi/data_login_api.php
@@ -1,8 +1,6 @@
 <?php
     // 前台網域名稱
-    header("Access-Control-Allow-Origin: https://tsaiweitse.github.io/clothes/");
-    // 後臺連結用變數
-    $apiLinks = 'https://twjzi14200.000webhostapp.com/api/';
+    header("Access-Control-Allow-Origin: https://tsaiweitse.github.io");
 
     $url = 'https://twjzi14200.000webhostapp.com/api/login_api.php';
     $thisData = file_get_contents($url);
