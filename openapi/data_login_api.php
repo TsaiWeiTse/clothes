3bbf706af11428d1874dc0c762a57b85f86e2681
twjzi14200 <twjzi14200@gmail.com>
2023-09-23 08:34:10 +0800

    20230923

--- openapi/data_login_api.php
+++ openapi/data_login_api.php
@@ -1,6 +1,6 @@
 <?php
     // 前台網域名稱
-    header("Access-Control-Allow-Origin: https://tsaiweitse.github.io");
+    header("Access-Control-Allow-Origin: *");
 
     $url = 'https://twjzi14200.000webhostapp.com/api/login_api.php';
     $thisData = file_get_contents($url);
